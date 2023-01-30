58951ff036cb74b9ddce28baa9eb1265063f35d7
lambiengcode <lambiengcode@gmail.com>
2023-01-30 14:44:48 +0700

    Bump 0.0.2

--- test/ios_device_name_test.dart
+++ test/ios_device_name_test.dart
@@ -1,12 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ios_device_name/ios_device_name.dart';
-
 void main() {
   test('adds one to input values', () {
-    final calculator = IOSDeviceName();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+    expect(1, 1);
   });
 }
